dbb0620ffcaf21255701c4aba2648af11b53c4b0
Jaime J.E. Yoon <6478127+jungeunyoon@users.noreply.github.com>
2020-04-28 22:25:10 +0900

    setup initial breeze api connection

---
+++ include/header.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('include/breeze.php');
+$breeze = new Breeze('b4e2479b3c57e0d08be71e27334f6c15');
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("include/header.php");
+
+?>
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+ </body>
+</html>
